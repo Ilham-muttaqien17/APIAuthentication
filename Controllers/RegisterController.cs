cc2377de23dc9e36119fa4acd9ec132141933b3e
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2021-01-08 20:57:49 +0700

    Add project files.

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using APIAuthentication.Models;
+using APIAuthentication.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        XamarinDatabaseContext XamarinDatabaseContext = new XamarinDatabaseContext();
+
+        [HttpPost]
+        public string Post([FromBody]TbUser value)
+        {
+            if(!XamarinDatabaseContext.TbUsers.Any(user => user.Username.Equals(value.Username)))
+            {
+                TbUser user = new TbUser();
+                user.Username = value.Username;
+                user.Salt = Convert.ToBase64String(Common.GetRandomSalt(16));
+                user.Password = Convert.ToBase64String(Common.SaltHashPassword(
+                    Encoding.ASCII.GetBytes(value.Password),
+                    Convert.FromBase64String(user.Salt)));
+
+                try
+                {
+                    XamarinDatabaseContext.Add(user);
+                    XamarinDatabaseContext.SaveChanges();
+                    return JsonConvert.SerializeObject("Register Succesfully");
+                }
+                catch (Exception ex)
+                {
+                    return JsonConvert.SerializeObject(ex.Message);
+                }
+            } else
+            {
+                return JsonConvert.SerializeObject("User is existing in Database");
+            }
+        }
+    }
+}
---
+++ Utils/Common.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace APIAuthentication.Utils
+{
+    public class Common
+    {
+        public static byte[] GetRandomSalt(int length)
+        {
+            var random = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[length];
+            random.GetNonZeroBytes(salt);
+            return salt;
+        }
+
+        public static byte[] SaltHashPassword(byte[] password, byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA256Managed();
+            byte[] plainTextWithSaltByte = new byte[password.Length + salt.Length];
+
+            for(int i = 0; i < password.Length; i++)
+            {
+                plainTextWithSaltByte[i] = password[i];
+            }
+
+            for(int i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltByte[password.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltByte);
+        }
+    }
+}
